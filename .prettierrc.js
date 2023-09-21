ed23eff89deb2087ce6d1510688fbbb4ffa8d4b2
Wlad Paiva <desenvolvedormg@gmail.com>
2023-09-21 09:05:46 -0300

    install prettier

---
+++ .prettierrc.js
@@ -0,0 +1,21 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: false,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxSingleQuote: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
